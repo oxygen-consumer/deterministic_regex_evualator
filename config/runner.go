cf5d77ad935afb8496bb5faf9c669e93ccd7c299
Stefan Zaharie <stefan@zaha.tech>
2025-04-28 19:40:14 +0300

    cleanup

--- config/runner.go
+++ config/runner.go
@@ -2,45 +2,20 @@ package config
 
 import (
 	"deterministic_regex_evaluator/dfa"
-	"deterministic_regex_evaluator/nfa"
-	"deterministic_regex_evaluator/regex"
 	"fmt"
-	"os"
 )
 
-func RunTests(tests []RegexTest) error {
-	for _, test := range tests {
-		tokens := regex.Tokenize(test.Regex)
+func RunTest(test RegexTest, dfaa *dfa.DFA) {
+	ok := true
 
-		postfix, err := regex.Parse(tokens)
-		if err != nil {
-			return err
+	for _, testString := range test.TestStrings {
+		if dfa.RunDFA(dfaa, testString.Input) != testString.Expected {
+			ok = false
+			fmt.Printf("\tFAIL: %s\n", testString.Input)
 		}
-
-		var postfixRunes []rune
-		for _, tok := range postfix {
-			postfixRunes = append(postfixRunes, tok.Value)
-		}
-
-		fmt.Printf("test %s: %s -> %s\n", test.Name, test.Regex, string(postfixRunes))
-
-		builtNFA, err := nfa.BuildNFA(postfix)
-		if err != nil {
-			return err
-		}
-
-		builtDFA := dfa.BuildDFAFromNFA(builtNFA)
-		for _, testString := range test.TestStrings {
-			if testString.Expected != dfa.RunDFA(builtDFA, testString.Input) {
-				fmt.Printf("test failed for test %s on input %s\n", test.Name, testString.Input)
-			}
-		}
-
-		dotStringNFA := builtNFA.ToDot()
-		os.WriteFile("./out/"+test.Name+"_nfa.dot", []byte(dotStringNFA), 0644)
-		dotStringDFA := builtDFA.ToDot()
-		os.WriteFile("./out/"+test.Name+"_dfa.dot", []byte(dotStringDFA), 0644)
 	}
 
-	return nil
+	if ok {
+		fmt.Printf("\tPASS\n")
+	}
 }
--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"deterministic_regex_evaluator/config"
+	"deterministic_regex_evaluator/dfa"
+	"deterministic_regex_evaluator/nfa"
+	"deterministic_regex_evaluator/regex"
 	"flag"
 	"fmt"
 	"os"
@@ -24,9 +27,30 @@ func main() {
 		os.Exit(1)
 	}
 
-	err = config.RunTests(tests)
-	if err != nil {
-		fmt.Printf("Error: %s\n", err)
-		os.Exit(1)
+	for _, test := range tests {
+		fmt.Printf("Test %s: %s\n", test.Name, test.Regex)
+
+		builtDFA, err := regexToDFAWithSubsetConstruction(test.Regex)
+		if err != nil {
+			fmt.Printf("\tERROR: %s\n", err)
+		}
+
+		config.RunTest(test, builtDFA)
 	}
+}
+
+func regexToDFAWithSubsetConstruction(regexx string) (*dfa.DFA, error) {
+	tokens := regex.Tokenize(regexx)
+
+	postfix, err := regex.Parse(tokens)
+	if err != nil {
+		return nil, fmt.Errorf("failed to convert to postfix: %s", err)
+	}
+
+	builtNFA, err := nfa.BuildNFA(postfix)
+	if err != nil {
+		return nil, fmt.Errorf("failed to build nfa: %s", err)
+	}
+
+	return dfa.BuildDFAFromNFA(builtNFA), nil
 }
